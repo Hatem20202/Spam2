f8f67d2583ab2a697c58925a2c351bcdd8e1768a
Hatem20202 <65527022+Hatem20202@users.noreply.github.com>
2020-05-23 07:41:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const myid = ['Your ID']
+const myid = ['467778292032864257']
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const client3 = new Discord.Client();
